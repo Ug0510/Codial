82fdc980acab05c1585d53e577b5f6277701c662
Ug0510 <uditgg0510@gmail.com>
2023-08-31 23:02:29 +0530

    set up view engine directory

--- index.js
+++ index.js
@@ -2,18 +2,21 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-//use express router
+// use express router
 app.use('/',require('./routes'));
 
+// setting up ejs for view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port , function(err){
     if(err)
     {
-        //equivalent to console.log('Error',err);
-        //this is called interpolation
+        // equivalent to console.log('Error',err);
+        // this is called interpolation
         console.log(`Error in running the server: ${err}`);
     }
 
-    //if server runs 
+    // if server runs 
     console.log(`Server is running on port ${port}`);
 })
