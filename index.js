a4d3a8d4f1d6c6f84bdb977ad465f93104429922
Ug0510 <uditgg0510@gmail.com>
2023-09-03 23:09:04 +0530

    setup static files acces for layouts

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
-// use express router
-app.use('/',require('./routes'));
 
 app.use(express.static('./assets'));
 app.use(expressLayouts);
 
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 
-// app.set('layout extractStyles',true);
+
+// use express router
+app.use('/',require('./routes'));
+
 
 // setting up ejs for view engine
 app.set('view engine','ejs');
