fe0a70e3beadc3d0be8f1fb6d26b76e5a9a56fb3
Ug0510 <uditgg0510@gmail.com>
2023-09-03 22:37:23 +0530

    Added layout

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 8000;
-const expressLayouts = require('express-ejs-layout');
-
-
+const expressLayouts = require('express-ejs-layouts');
 // use express router
 app.use('/',require('./routes'));
-app.use(expressLayouts);
-app.use(express.static('./assests'));
 
-app.set('layout extractStyles',true);
+app.use(express.static('./assets'));
+app.use(expressLayouts);
+
+
+// app.set('layout extractStyles',true);
 
 // setting up ejs for view engine
 app.set('view engine','ejs');
