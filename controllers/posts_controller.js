4abc98fea580e7699e10c4a328916fd8f3deb8f9
Ug0510 <uditgg0510@gmail.com>
2023-09-26 21:48:33 +0530

    Post getting added in database on submitting

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,14 +1,18 @@
 const Post = require('../models/post');
 
-module.exports.create = function(req,res){
-    Post.create({
-        content: req.body.content,
-        user: req.user._id
-    }, function(err){
-        if(err)
-        {
-            console.log(err);
-        }
+module.exports.create = async function(req, res) {
+    try {
+        const newPost = await Post.create({
+            content: req.body.content,
+            user: req.user._id
+        });
+        
+        // Handle the case where the post is created successfully
+        // You can choose to redirect to a different page or do something else here.
+        // For example, you can redirect to the newly created post's page.
+        return res.redirect(`back`);
+    } catch (err) {
+        console.error(err);
         return res.redirect('back');
-    })
+    }
 };
