c330a8fd73958996caace5ff5dd1810ee9b98f66
Ug0510 <uditgg0510@gmail.com>
2023-10-24 15:54:21 +0530

    hiding password from data shown by api request

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,24 @@
-module.exports.index = function(req , res){
+const Post = require('../../../models/post');
+
+module.exports.index = async function(req , res){
+
+    const posts = await Post.find({})
+    .sort('-createdAt')
+    .populate({
+        path: 'user',
+        select: 'name email createdAt updatedAt avatar'
+    })
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user',
+            select: 'email name createdAt updatedAt avatar'
+        },
+        options: { sort: { createdAt: -1 } }
+    });
+
     return res.json(200, {
         message: "List post",
-        posts: []
+        posts: posts
     })
 }
